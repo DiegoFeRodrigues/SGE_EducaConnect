010a2c45894b3fcdcdd3ea0dd6d161df006418f9
DiegoFeRodrigues <dirod23@live.com>
2025-10-11 17:19:19 -0300

    alterando Aluno

--- Aluno.java
+++ Aluno.java
@@ -1,3 +1,25 @@
 public class Aluno {
 
+    private String nome, curso;
+    private int matricula;
+    
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }    
 }
