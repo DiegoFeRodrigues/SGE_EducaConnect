0d77c711e38e48dd8b270cdf8e49c64a10344e95
DiegoFeRodrigues <dirod23@live.com>
2025-10-17 14:34:56 -0300

    FASE DOIS FINAL - teste remoção commit

--- Turma.java
+++ Turma.java
@@ -6,8 +6,8 @@ public class Turma {
     private Curso codigo;
     private Curso nomeCurso;
     private Professor professor;
-    private List<Aluno> listaAlunos;
-    
+    private List<String> listaAlunos;
+    // constructor:
     public Turma(Curso codigo, Curso nomeCurso, Professor professor) {
         this.codigo = codigo;
         this.nomeCurso = nomeCurso;
@@ -15,22 +15,30 @@ public class Turma {
         this.listaAlunos = new ArrayList<>();
     }
 
-    public Curso getCursoTur() {
-        return nomeCurso;
-    }
-
+    // método adiciona aluno: 
     public void addAluno(Aluno aluno, Curso curso) {
         if (aluno.getCursoAlu().equals(curso.getNomeCurso())) {
-            listaAlunos.add(aluno);
+            listaAlunos.add(aluno.getNomeAlu());
+            System.out.println(aluno.getNomeAlu()+
+            " foi adicionado ao curso de "+curso.getNomeCurso()+".");
         }
+    }
+    // método remove aluno:
+    public void removeAluno(Aluno aluno, Curso curso) {
+        if (aluno == null){
+            System.out.println("Aluno inválido!");
+        }else {
+            listaAlunos.remove(aluno.getNomeAlu());
+                System.out.println(aluno.getNomeAlu()+" foi removido.");
+            }
     }
     
     // saída turma:
     @Override
     public String toString() {
-        return "\n\nDados da turma:"+
+        return "\nDados da turma do curso de "+this.nomeCurso.getNomeCurso()+":"+
         "\nProfessor: "+this.professor.getNomeProf()+
         "\nCurso: "+this.nomeCurso.getNomeCurso()+
-        "\nQuantidade de alunos:"+listaAlunos.size();
+        "\nQuantidade de alunos: "+listaAlunos.size()+" - "+listaAlunos;
     }
 }
