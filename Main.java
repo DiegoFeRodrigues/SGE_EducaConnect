1dd653fd6264c01ed05f83338ad6ef59c8074f26
DiegoFeRodrigues <dirod23@live.com>
2025-10-17 15:08:01 -0300

    FASE DOIS FINAL?

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
         Aluno aluno1 = new Aluno("Diego", "ADS", 12345);
         Aluno aluno2 = new Aluno("Kuririn", "Artes marciais", 18);
         Aluno aluno3 = new Aluno("Kakarotto", "Artes marciais", 59);
-        
+        Aluno aluno4 = new Aluno("Yamcha", "Artes marciais", 7);
         // cursos:
         Curso curso1 = new Curso("ADS", 54321, 2180f);
         Curso curso2 = new Curso("Artes marciais", 98765, 3865.3f);
@@ -15,14 +15,21 @@ public class Main {
         Turma tur1 = new Turma(curso1, curso1, professor1);
         Turma tur2 = new Turma(curso2, curso2, professor2);
 
-        // saída:
-        // System.out.println(aluno1.toString()+curso1.toString()+
-        // professor1.toString()+"\n\n"+aluno2.toString()+curso2.toString()+
-        // professor2.toString()+tur1.toString()+tur2.toString());
+        /* saída:
+        System.out.println(aluno1.toString()+curso1.toString()+
+        professor1.toString()+"\n\n"+aluno2.toString()+curso2.toString()+
+        professor2.toString()+tur1.toString()+tur2.toString()); */
 
+        // adicionando aluno em seus cursos:
+        System.out.println("Alunos adicionados:");
         tur1.addAluno(aluno1, curso1);
         tur2.addAluno(aluno2, curso2);
         tur2.addAluno(aluno3, curso2);
+        tur2.addAluno(aluno4, curso2);
+
+        // removendo aluno:
+        System.out.println("\nAlunos removidos:");
+        tur2.removeAluno(aluno4, curso2);
 
         System.out.println(tur1.toString()+"\n"+tur2.toString());
         
