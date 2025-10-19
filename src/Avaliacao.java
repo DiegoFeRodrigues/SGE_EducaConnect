4c1e173e6ff725697c9360ff9fb8b7db058692a1
DiegoFeRodrigues <dirod23@live.com>
2025-10-19 09:54:26 -0300

    movendo fasetres para estrutura de arquivos

--- src/Avaliacao.java
+++ src/Avaliacao.java
@@ -1,3 +1,4 @@
+package src;
 public class Avaliacao {
     // atributos:
     private float nota;
--- src/Curso.java
+++ src/Curso.java
@@ -1,3 +1,4 @@
+package src;
 public class Curso {
     // atributos:
     private String nomeCurso;
